011273ece058e2d59720c7bc3a24dd15875b0dd6
Josh Rosenberg <jrosenberg@communally.tech>
2019-03-19 17:22:16 -0400

    the rest of the REs

--- coding-assignment/cc-validation.py
+++ coding-assignment/cc-validation.py
@@ -4,16 +4,34 @@ import re, sys
 
 def looksvalid(ccnum):
     #Note: LOOKS valid. Like, to someone who hasn't read the assignment.
-    if re.match('\d{16}$', ccnum):
+    if re.match(r'\d{16}$', ccnum):
         return True
-    if re.match('(\d{4}-){3}\d{4}$', ccnum):
+    if re.match(r'(\d{4}-){3}\d{4}$', ccnum):
         return True
     return False
+
+def leadingdigitok(ccnum):
+    if re.match(r'[456]', ccnum):
+        return True
+    return False
+def toomanyconsecutive(ccnum):
+    # note reverse sense from previous functions
+    if re.search(r'(\d)\1\1\1', ccnum):
+        return True
+    return False
+
 
 if __name__ == "__main__":
     for ccnum in map(str.rstrip, sys.stdin):
         if not looksvalid(ccnum):
             print("Invalid")
             continue
-        # More stuff here
+        if not leadingdigitok(ccnum):
+            print("Invalid")
+            continue
+        ccnum = re.sub(r'-', '', ccnum)
+        if toomanyconsecutive(ccnum):
+            # note reverse sense from previous calls
+            print("Invalid")
+            continue
         print("Valid")
